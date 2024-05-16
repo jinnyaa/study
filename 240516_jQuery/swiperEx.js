350e7021137147f46fdfe548992ad1cb217f3fa7
jinnyaa <oio99661@gmail.com>
2024-05-16 17:53:54 +0900

    240516 파일 업로드 및 assets 업데이트

---
+++ 240516_jQuery/swiperEx.js
@@ -0,0 +1,17 @@
+const mainSwiper = new Swiper(".main-swiper", {
+    speed: 600,
+    loop:true,
+    parallax: true,// 시간차 움직임
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
